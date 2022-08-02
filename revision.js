880ea0db5837a704388f3970b69e1a6758e6845e
Masum Ahmed <creatormasum@gmail.com>
2022-08-03 04:25:40 +0600

    revesion js

--- revision.js
+++ revision.js
@@ -1,3 +1,14 @@
+
+// String Type Variable 
 var bottle = 'water';
 var bottleColor = 'yellow';
+
+
+// Number Type Variable 
 var sunglassPrice = 120;
+
+
+// Boolean Type Varibale 
+var isExpensive = true;
+
+var tableItems = ['bottle', 'pen', 'mouse', 'sunglass'; 'paper']
