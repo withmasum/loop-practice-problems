d6380b6f9fc628a5477c7f7d5d7b2d31cae2a97c
Masum Ahmed <creatormasum@gmail.com>
2022-08-03 04:29:22 +0600

    revision js done

--- revision.js
+++ revision.js
@@ -11,4 +11,27 @@ var sunglassPrice = 120;
 // Boolean Type Varibale 
 var isExpensive = true;
 
-var tableItems = ['bottle', 'pen', 'mouse', 'sunglass'; 'paper']
+
+
+var tableItems = ['bottle', 'pen', 'mouse', 'sunglass'; 'paper'];
+
+var item3 = tableItems[3]
+tableItems[4] = 'notebook';
+var penIndex = tableItems.indexOf('pen')
+
+//push pop
+
+
+// conditionslas
+
+if (tableItems.length > 4) {
+
+}
+
+else if (tableItems[3] == 'pen') {
+
+}
+
+else {
+
+}
