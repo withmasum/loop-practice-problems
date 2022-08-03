d590e0d26b24c5b979a1a66bdade9d2386898de8
Masum Ahmed <creatormasum@gmail.com>
2022-08-03 17:13:36 +0600

    for js

---
+++ even.js
@@ -0,0 +1,7 @@
+// Target 1 to 10 all even number 
+var number = 0;
+while (number <= 10) {
+    console.log(number)
+    // number++;
+    number = number + 2;
+}
---
+++ for.js
@@ -0,0 +1,12 @@
+var roatGiven = 0;
+while (roastGiven < 7) {
+    console.log('Roast Den, Please!!')
+    roastGiven++;
+
+}
+
+// For Loop
+
+for (var roastGiven = 0; roastGiven < 7; roastGiven++) {
+    console.log('Roast Den, Please !!!');
+}
