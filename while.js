2b06f1dfe5a43b6188b7d3e573e12b9d801a8a6d
Masum Ahmed <creatormasum@gmail.com>
2022-08-03 17:01:47 +0600

    hello

---
+++ while.js
@@ -0,0 +1,15 @@
+// if(condition){
+
+//}
+
+
+
+var roastGiven = 0;
+while (roastGiven < 7) {
+
+    // roastGiven = roastGiven + 1;
+    // roastGiven++;
+    console.log('Roast Den Please!!')
+    roastGiven = roastGiven + 1;
+
+}
