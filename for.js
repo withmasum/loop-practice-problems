78d20005d1aaa285a77ee8ae8fb07ea3c12ae2e2
Masum Ahmed <creatormasum@gmail.com>
2022-08-03 17:18:58 +0600

    masum

--- for.js
+++ for.js
@@ -10,3 +10,11 @@ while (roastGiven < 7) {
 for (var roastGiven = 0; roastGiven < 7; roastGiven++) {
     console.log('Roast Den, Please !!!');
 }
+
+
+// For Loop more simplified 
+// Simple or usual version 
+console.log('simple for loop');
+for (var i = 0; i < 7; i++) {
+    console.log(i);
+}
