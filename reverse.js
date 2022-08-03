636273022cf8527cbab40d6cbba3fba9c22190e0
Masum Ahmed <creatormasum@gmail.com>
2022-08-03 18:20:47 +0600

    reverse js

---
+++ reverse.js
@@ -0,0 +1,31 @@
+/* var number = 1;
+while (number <= 10) {
+    console.log(number);
+    number++;
+} */
+
+
+
+
+// While Loop reverse way 
+/* var num = 10;
+while (num >= 1) {
+    console.log(num);
+    num--;
+}
+ */
+
+var num = 10;
+while (num >= 1) {
+
+    num--;
+}
+
+for (var i = 1; i <= 10; i++) {
+
+    // console.log(i);
+}
+
+for (var i = 10; i > 1; i--) {
+    console.log(i);
+}
